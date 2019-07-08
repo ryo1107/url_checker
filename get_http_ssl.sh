6a766252182d1acb1b81fcaec5df8510a2b6652d
develop35 <develop35@develop35-no-iMac.local>
2019-07-08 16:08:53 +0900

    add func curl Cache-clear

--- get_http_ssl.sh
+++ get_http_ssl.sh
@@ -6,7 +6,7 @@ do
         url=${line#*//}
         domain=${url%%/*}
         #echo $domain
-        http_code+=($(curl -LI $line -o /dev/null -w '%{http_code}\n' -s),)
+        http_code+=($(curl -H 'Cache-Control: no-cache' -LI $line -o /dev/null -w '%{http_code}\n' -s),)
         ssl_result+=($(bash check_ssl_certs.sh $domain 2>/dev/null | tail -n 2 |grep Verify),)
 
 done < ./url_list.txt
